4d45f9c516c2aaf817a94c13544727273faa2f6e
Muftawo <hajjmuf@gmail.com>
2022-08-03 11:35:16 +0000

    rustbook chapter 3 working with intergers

--- RustBook/chapter_3/data_types/src/main.rs
+++ RustBook/chapter_3/data_types/src/main.rs
@@ -1,6 +1,23 @@
 fn main() {
     println!("Rust Data Types!");
-    println!("Rust Scalar and and Compound Types!");
+    println!("Rust Scalar and Compound Types!");
+
+    // Understanding scalar types
+    // Rust has four scalar types : Integers | floats | string | boolean
+
+    println!("Integers");
+    let x: u8 = u8::pow(2, 7)-1 ;
+    println!("the value of x is {}",x);
+    // let y: i8 = i8::pow(2, 8)-1;
+    // println!("the value of y is {}",y);
+    let z: u32 = u32::pow(2, 31) - 1;
+    println!("the value of z is {}",z);
+    // let a: i32 = i32::pow(2, 32);
+    // println!("the value of a is {}",a);
+    let b: u64 = u64::pow(2, 63) - 1;
+    println!("the value of b is {}",b);
+    // let c: i64 = 45;
+    // println!("the value of c is {}",c);
 
     // 
 }
