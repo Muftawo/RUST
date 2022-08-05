9f7529e9b97691d1e91a5d36ccfbfaf3eaa175c6
Muftawo <hajjmuf@gmail.com>
2022-08-05 06:19:47 +0000

    if condition else

---
+++ RustBook/chapter_3/branches/src/main.rs
@@ -0,0 +1,9 @@
+    let number = 3;
+    // shadow number to 7
+    let number= number + 4 ;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
