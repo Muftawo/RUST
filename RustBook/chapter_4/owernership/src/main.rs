2f6a9d10729db99486d090de13001f18232380ad
Muftawo <hajjmuf@gmail.com>
2022-09-26 02:40:11 +0000

    chapter 4

---
+++ RustBook/chapter_4/owernership/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+
+    // let mut s = String::from("hello");
+    // s.push_str("world!");
+
+    fn char() {
+        let c = 'z';
+        let z = 'ℤ';
+        let heart_eyed_cat = '😻';
+        let x = z ;
+
+        println!("{c},{z},{heart_eyed_cat},z {x} equivalent")
+    }
+
+    char();
+
+
+}
